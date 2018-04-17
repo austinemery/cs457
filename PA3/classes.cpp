6484430f4dd2fbe0dd189a3f2be78b77aeaf9dea
mmanderson009 <mercedesanderson@nevada.unr.edu>
2018-04-16 20:07:46 -0700

    Here you go nick.

--- PA3/classes.cpp
+++ PA3/classes.cpp
@@ -748,10 +748,32 @@ int Database::getTableIndex( string givenTable )
 }
 void Database::innerJoin( string joinSelection , string leftTableName , string rightTableName , string leftAtt , string rightAtt )
 {
-
 	int indexLeftTable = getTableIndex( leftTableName );
 	int indexRightTable = getTableIndex( rightTableName );
 	int indexLeftAtt = tableData[indexLeftTable].getMetaIndex( leftAtt );
 	int indexRightAtt = tableData[indexRightTable].getMetaIndex( rightAtt );
+
+	for( int index = 0 ; index < tableData[indexLeftTable].numbAtt ; index++ )
+	{
+		cout << tableData[indexLeftTable].metaData[index] << " | ";
+	}
+	for( int jndex = 0 ; jndex < tableData[indexRightTable].numbAtt ; jndex++ )
+	{
+
+		if( jndex != ( tableData[indexRightTable].numbAtt-1 ))
+		{
+			cout << tableData[indexRightAtt].metaData[jndex] << " | ";
+		}else
+		{
+			cout << tableData[indexRightAtt].metaData[jndex];
+		}
+	}
+
+
+	//for the attribute of interest in the left table
+		//for the attribute of interest in the right table
+			//If the right tuple at the rightAtt == left tupe at the leftAtt
+				//Print both tuples
+
 
 }
